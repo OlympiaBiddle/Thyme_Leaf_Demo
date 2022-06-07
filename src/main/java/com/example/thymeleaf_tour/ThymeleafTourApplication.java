54d0a8f0cfa5d81889d96b433e5eb513b44af6ee
OlympiaBiddle <osb399@gmail.com>
2022-06-07 12:01:01 -0400

    Added html pages and made changes to the package to scan all packages in the folder

--- src/main/java/com/example/thymeleaf_tour/ThymeleafTourApplication.java
+++ src/main/java/com/example/thymeleaf_tour/ThymeleafTourApplication.java
@@ -2,8 +2,9 @@ package com.example.thymeleaf_tour;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"mycontroller"})
 public class ThymeleafTourApplication {
 
 	public static void main(String[] args) {
